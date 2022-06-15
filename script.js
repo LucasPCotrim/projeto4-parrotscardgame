4bad97d7b7bd6e7ae817a99d76b49a3525ad4234
lucas_cotrim <lucas_cotrim@tpn.usp.br>
2022-06-15 16:19:22 -0300

    Implemented main game logic, correct guess and incorrect guess delay

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ const gif_indexes = [0,1,2,3,4,5,6]; // array of gif indexes from 'gif_paths'
 let number_of_cards; // '4, 6, 8, 10, 12, 14'
 let chosen_gif_indexes; // array of length 'number_of_cards' containing non-repeating paired indexes from 0 to 6.
 let cards_states;   // 'face_down', 'face_up_guessing', 'face_up_correct'
-
+let number_of_plays; // number of times user has clicked to flip a card
 
 // DOM objects
 const DOM_card_container = document.querySelector('.card_container');
@@ -69,11 +69,11 @@ function fill_cards() {
 
     for (let i = 0; i < number_of_cards; i++) {
         DOM_card_container.innerHTML += `
-                                    <div class="card" data-identifier="card" onclick="flip_card(${i})">
-                                        <div class="card_back_face">
+                                    <div class="card" data-identifier="card" onclick="update_board_state(${i})">
+                                        <div class="card_back_face" data-identifier="back-face">
                                             <img src="imgs/front.png">
                                         </div>
-                                        <div class="card_front_face hidden">
+                                        <div class="card_front_face hidden" data-identifier="front-face">
                                             <img src="${gif_paths[chosen_gif_indexes[i]]}">
                                         </div>
                                     </div>
@@ -85,11 +85,30 @@ function fill_cards() {
 
 
 function initialize_game() {
+
+    fill_cards();
+
+    console.log('chosen_gif_indexes')
+    console.log(chosen_gif_indexes)
+
+    cards_states = [];
+
     for (let i = 0; i < number_of_cards; i++) {
         cards_states.push('face_down');
     }
+    number_of_plays = 0;
 }
 
+
+function count_occurrences_in_array(array, elem){
+    let cont = 0;
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] == elem){
+            cont ++;
+        }
+    }
+    return cont;
+}
 
 function flip_card(card_index) {
     DOM_cards[card_index].querySelector('.card_back_face').classList.toggle('hidden');
@@ -98,11 +117,61 @@ function flip_card(card_index) {
 
 
 
+function update_board_state(card_index) {
+
+    console.log('board_state_before:')
+    console.log('cards_states')
+    console.log(cards_states)
+
+    // Card clicked was actually face-down
+    if (cards_states[card_index] == 'face_down'){
+        number_of_plays += 1;
+
+        // 1 Card was already face-up (guessing)
+        if (count_occurrences_in_array(cards_states, 'face_up_guessing') == 1){
+
+            previous_guess_index = cards_states.indexOf('face_up_guessing')
+
+            // Correct Guess
+            if (chosen_gif_indexes[card_index] == chosen_gif_indexes[previous_guess_index]){
+                flip_card(card_index);
+                cards_states[previous_guess_index] = 'face_up_correct';
+                cards_states[card_index] = 'face_up_correct';
+            }
+            // Incorrect Guess (flip card, wait 1 sec then flip back both guesses)
+            else{
+                flip_card(card_index);
+                setTimeout(function(){
+                    flip_card(previous_guess_index);
+                    flip_card(card_index);
+                }, 1000);
+                cards_states[previous_guess_index] = 'face_down';
+                cards_states[card_index] = 'face_down';
+            }
+
+        }
+
+        // No card was being guessed
+        else if (count_occurrences_in_array(cards_states, 'face_up_guessing') == 0){
+            flip_card(card_index);
+            cards_states[card_index] = 'face_up_guessing';
+        }
+    }
+    console.log('board_state_before:')
+    console.log('cards_states')
+    console.log(cards_states)
+    
+}
+
+
+
+
+
 // -------------------------- Main --------------------------
 
 number_of_cards = ask_number_of_cards();
 chosen_gif_indexes = choose_gif_indexes_in_game();
-fill_cards();
+initialize_game();
 
 
 
